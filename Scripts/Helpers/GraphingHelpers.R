b55207de05363aa56ee97ba6a91f74c1563af8a9
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-04-26 00:30:09 +0200

    Moves functions around for clarity

---
+++ Scripts/Helpers/GraphingHelpers.R
@@ -0,0 +1,32 @@
+AddSpecialPaths = function(position_table, ls){
+  list_names = names(ls)
+  position_table = position_table[, special:= "normal"]
+  list_names = names(ls)
+  for (i in 1:length(ls)){
+    position_table = position_table[is_between(Time,ls[[i]]$start,ls[[i]]$finish), special:= list_names[i] ]
+  }
+  return(position_table)
+}
+SavePlot = function(inputPlot,name){
+  mypath <- paste("../images/", name, sep = "")
+  file = png(mypath, width = 1200, height = 800, units = "px")
+  plot(inputPlot)
+  dev.off()
+}
+MakeCircle <- function(center = c(0,0), radius = 1, precision = 100){
+  tt <- seq(0,2*pi,length.out = precision)
+  xx <- center[1] + radius * cos(tt)
+  yy <- center[2] + radius * sin(tt)
+  return(data.frame(x = xx, y = yy))
+}
+AddPointsToPlot = function(plot, ls){
+  list_names = names(ls)
+  data_table = data.frame(point.x=numeric(0),point.y=numeric(0), point.name=character(), stringsAsFactors = F)
+  for (i in 1:length(ls)){
+    data_table[i,1] = ls[[i]][1]
+    data_table[i,2] = ls[[i]][2]
+    data_table[i,3] = list_names[i]
+  }
+  plot = plot + geom_point(data = data_table, aes(point.x,point.y),size = 4, color = "blue") + geom_text(data = data_table, aes(point.x, point.y,label=point.name))
+  return(plot)
+}
--- Scripts/Helpers/UnityHelpers.R
+++ Scripts/Helpers/UnityHelpers.R
@@ -8,10 +8,21 @@ GetTrialTimewindow = function(test, trialID){
   ls$finish = (filter(test$data, Index == trialID & Sender == "Trial" & Event == "Finished") %>% select(Time))[[1]]
   return(ls)
 }
-
 SelectPlayerLog = function(playerLog, trialTimewindow){
   #checking for entirety
   log = playerLog[Time > trialTimewindow$start & Time < trialTimewindow$finish]
   #checking log
   return(log)
+}
+
+GetGoalIndex = function(trialID, test){
+  if(GetTrialType(test, trialID)=="Allo"){
+    uncorrectedIndex = test$experimentSettings$MarkOrder[trialID] + test$experimentSettings$AlloMarkRelation + 1 
+  } else {
+    uncorrectedIndex = test$experimentSettings$StartOrder[trialID] + test$experimentSettings$EgoMarkRelation + 1 
+  }
+  if(uncorrectedIndex>test$experimentSettings$NumberOfGoals) return(uncorrectedIndex-test$experimentSettings$NumberOfGoals) else return(uncorrectedIndex)
+}
+GetTrialType = function(test,trialID){
+  return(test$experimentSettings$RandomOrdering[trialID])
 }
