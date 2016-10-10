136990e3b9d1925c2a6fa3afc8b651d03204a42b
Lukáš Hejtmánek <hejtmy@gmail.com>
2016-10-10 14:55:24 +0200

    small changes to main

--- Main.R
+++ Main.R
@@ -1,9 +1,15 @@
 #give me folder
 source("Scripts/Loading.R")
 
-data_dir = "../Data"
+dataDir = "../Data"
 
-Analysis = UnityAnalysis$new(data_dir, "P108")
+Analysis = UnityAnalysis$new(dataDir, "p108")
 Analysis$TestResults()
 
 make_trial_images(Analysis$playerLog, Analysis$tests[[1]], indices = c(2:3))
+
+Analysis$playerLog$timeDiff = c(0, diff(Analysis$playerLog$Time))
+ggplot(Analysis$playerLog, aes(x = Time, y = distance/timeDiff)) + geom_line()
+
+hist(Analysis$playerLog$timeDiff)
+summary(Analysis$playerLog$timeDiff)
