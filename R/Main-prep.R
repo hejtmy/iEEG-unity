b882fd20832d75810d85cf85f14261e7eeaf89ef
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-04-03 13:37:21 +0200

    Basic preparation script for matlab

---
+++ R/Main-prep.R
@@ -0,0 +1,10 @@
+#give me folder
+source("R/Loading.R")
+
+data_dir = "../Data"
+
+Analysis = UnityAnalysis$new(data_dir, "ek_0102")
+Analysis$TestResults()
+
+Analysis$ExportSynchropulses()
+Analysis$ExportPlayerLog()
