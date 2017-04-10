32648199379b1e78fb1f313b0d05c0fa372562a7
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-04-10 14:39:26 +0200

    renamed svg-add start not to mix with other functions

--- R/Graphing/svg/elements/svg-add-start.R
+++ R/Graphing/svg/elements/svg-add-start.R
@@ -1,4 +1,4 @@
-add_start = function(plot, test, trialID){
+svg_add_start = function(plot, test, trialID){
   i_start = get_start_index(test, trialID)
   start_pos = get_start_positsions(test, i_start)
   plot = plot + geom_point(data = start_pos, aes(Position.x, Position.z), size = 10, color = "red")
