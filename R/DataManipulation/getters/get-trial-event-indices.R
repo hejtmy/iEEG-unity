67e3e9125826dc190e757469954e8df56cc8b781
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-05-23 18:51:24 +0200

    Fixes wording

--- R/DataManipulation/getters/get-trial-event-indices.R
+++ R/DataManipulation/getters/get-trial-event-indices.R
@@ -1,4 +1,4 @@
 get_trial_event_indices = function(test, event){
-  indexes = unique(filter(test$data, Sender == "Trial" & Event == event) %>% select(Index))[[1]]
-  return(indexes + 1)
+  indices = unique(filter(test$data, Sender == "Trial" & Event == event) %>% select(Index))[[1]]
+  return(indices + 1)
 }
