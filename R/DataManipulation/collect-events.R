57178e35f14306005d366f9e8f3abb72d427b224
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-05-31 15:26:14 +0200

    Redoes event outputting to address forced finished trials

--- R/DataManipulation/collect-events.R
+++ R/DataManipulation/collect-events.R
@@ -3,11 +3,15 @@
 #' @param name: string with the name of the event, e.g. ArduinoPulseStop
 #' @param path: where to save the file. Defaults ot hte workig path/exports
 collect_events <- function(test, dt_player){
-  trialIDs <- get_trial_event_indices(test, "Finished")
-  synchropulses <- test$data  %>% filter(Sender == "Trial") %>% filter(Event == "ArduinoPulseStart") %>% .$Time
-  trialSetup <- test$data %>% filter(Sender == "Trial") %>% filter(Event == "WaitingToStart") %>% .$Time
-  trialStarted <- test$data %>% filter(Sender == "Trial") %>% filter(Event == "ArduinoPulseStart") %>% .$Time
-  trialEnded <- test$data %>% filter(Sender == "Trial") %>% filter(Event == "Finished") %>% .$Time
+  iFinished = get_trial_event_indices(test, "Finished")
+  trialIDs = iFinished[!(iFinished %in% get_trial_event_indices(test, "ForceFinished"))]
+  
+  finishedTrials <- test$data %>% filter(Index %in% (trialIDs-1))
+  
+  synchropulse <- finishedTrials  %>% filter(Sender == "Trial") %>% filter(Event == "ArduinoPulseStart") %>% .$Time
+  trialSetup <- finishedTrials %>% filter(Sender == "Trial") %>% filter(Event == "WaitingToStart") %>% .$Time
+  trialStarted <- finishedTrials %>% filter(Sender == "Trial") %>% filter(Event == "Runing") %>% .$Time
+  trialEnded <- finishedTrials %>% filter(Sender == "Trial") %>% filter(Event == "Finished") %>% .$Time
   pointingStarted <- trialStarted
   pointingEnded <- c()
   pointingError <- c()
@@ -18,5 +22,5 @@ collect_events <- function(test, dt_player){
     pointingError <- c(pointingError, angle_difference(pointing$target, pointing$chosen))
     type <- c(type, get_trial_type(test, trialID))
   }
-  df = data.frame(trialIDs, trialSetup, trialStarted, trialEnded, pointingStarted, pointingEnded, pointingError, type)
+  df = data.frame(trialIDs, trialSetup, trialStarted, trialEnded, pointingStarted, pointingEnded, pointingError, synchropulse, type)
 }
