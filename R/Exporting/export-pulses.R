e8f68f652a645f5120024cabaeeb528d70299d0c
Lukáš Hejtmánek <hejtmy@gmail.com>
2018-10-04 18:25:37 +0200

    adds exporting functions

--- R/Exporting/export-pulses.R
+++ R/Exporting/export-pulses.R
@@ -2,13 +2,12 @@
 #'
 #' @param name: string with the name of the event, e.g. ArduinoPulseStop
 #' @param path: where to save the file. Defaults ot hte workig path/exports
-export_pulses = function(testData, event_name, id, path = getwd()){
+export_pulses <- function(obj, event_names, filename, path = getwd()){
   # VALIDATIONS
   
-  pulses = testData %>% filter(Event == event_name) %>% select(Time)
-  
-  df = data.frame(eventName = rep(event_name, length(pulses)), times = pulses)
-  
-  filePath = paste(path, "/", id, ".pulses", sep = "")
-  write.table(df, filePath, sep = ";", quote = F, row.names = F)
+  exp_data <- get_experiment_log(obj)
+  pulses <- exp_data %>% filter(Event %in% event_names) %>% select(Event, Time)
+  colnames(pulses) <- c("eventName", "times")
+  filePath <- paste(path, "/", filename, ".pulses", sep = "")
+  write.table(pulses, filePath, sep = ";", quote = F, row.names = F)
 }
--- R/Graphing/elements/add_goal.R
+++ R/Graphing/elements/add_goal.R
@@ -1,9 +1,10 @@
-add_goal = function(plot, test, trialID){
-  i_goal = get_goal_index(test, trialID) 
-  goal_pos = get_goal_position(test, i_goal)
-  goal_area = make_circle(get_goal_position(test, i_goal, onlyXY = T), test$experimentSettings$GoalSize, precision = 100)
+add_goal <- function(plot, obj, trialID){
+  i_goal <- get_goal_index(obj, trialID) 
+  goal_pos <- get_goal_position(obj, i_goal)
+  goal_area <- make_circle(get_goal_position(obj, i_goal, onlyXY = T), get_experiment_settings(obj)$GoalSize, precision = 100)
   #draws goal
-  plot = plot +
+  plot <- plot +
     geom_point(data = goal_pos, aes(Position.x, Position.z), size = 3, color = "red") +
     geom_path(data = goal_area, aes(x, y), color = "red")
+  return(plot)
 }
